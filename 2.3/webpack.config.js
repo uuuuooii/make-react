7b320e6bab82cdf68d08fd0daf0a9465de69b9ea
neo@zillinks.com <keunhey0107@gmail.com>
2023-08-08 00:33:26 +0900

    webpack 설정

--- 2.3/webpack.config.js
+++ 2.3/webpack.config.js
@@ -1,11 +1,28 @@
+const path = require('path');
+
 module.exports = {
   // 순서대로 실행
 
   mode: '',
   entry: '',
-  output: {},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
 
-  module: {},
+  module: {
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          option: {
+            // 바벨의 플로그인을 세팅
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 
   plugins: {},
 };
