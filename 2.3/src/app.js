0cc99e41945c49b443cf55b6e231dd05e6b0eaf1
neo@zillinks.com <keunhey0107@gmail.com>
2023-08-11 00:02:41 +0900

    Update: jsx 제작 시작

--- 2.3/src/app.js
+++ 2.3/src/app.js
@@ -15,6 +15,17 @@ const vdom = createElement(
   )
 );
 
+const vdom2 = (
+  <p>
+    <h1>React 만들기</h1>
+    <ul>
+      <li style='color: red'>첫 번째 아이템</li>
+      <li style='color: blue'>두 번째 아이템</li>
+      <li style='color: green'>세 번째 아이템</li>
+    </ul>
+  </p>
+);
+
 console.log(vdom);
 // UI에 보여주기
 render(vdom, document.querySelector('#root'));
