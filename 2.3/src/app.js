decdc9debdc50e9500a6078b0b205574934eefc5
neo@zillinks.com <keunhey0107@gmail.com>
2023-08-15 00:15:07 +0900

    Update: 함수 컴포넌트 만들기

--- 2.3/src/app.js
+++ 2.3/src/app.js
@@ -1,17 +1,21 @@
 /* @jsx createElement */
-import { createDOM, render, createElement } from './react';
+import { render, createElement } from './react';
 
 function Title(props) {
   return <h1>{props.children}</h1>;
+}
+
+function Item(props) {
+  return <li style={`color: ${props.color}`}>{props.children}</li>;
 }
 
 const vdom = (
   <p>
     <Title>React 만들기</Title>
     <ul>
-      <li style='color: red'>첫 번째 아이템</li>
-      <li style='color: blue'>두 번째 아이템</li>
-      <li style='color: green'>세 번째 아이템</li>
+      <Item color='red'>첫 번째 아이템</Item>
+      <Item color='blue'>두 번째 아이템</Item>
+      <Item color='green'>세 번째 아이템</Item>
     </ul>
   </p>
 );
--- 2.3/src/react.js
+++ 2.3/src/react.js
@@ -30,7 +30,7 @@ export function createElement(tag, props, ...children) {
     if (children.length > 0) {
       return tag({
         ...props,
-        children: children,
+        children: children.length === 1 ? children[0] : children,
       });
     } else {
       return tag(props);
